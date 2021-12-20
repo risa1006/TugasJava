ac7bdf0462443ef12d7de18e0ff4512fc897b74b
risa1006 <risa@unida.ac.id>
2021-12-20 15:35:25 +0700

    TUGAS JAVA
    Tugas alpro

---
+++ src/AnakAyam.java
@@ -0,0 +1,21 @@
+
+public class AnakAyam {
+	public static void main(String[] args) {
+		String ayam = " Anak ayam turun ";
+		int jumlah = 10;
+
+		System.out.println("Anak ayam turun " + jumlah);
+
+		while(jumlah > 1) {
+			System.out.println("Anak ayam turun " + jumlah + " mati satu tinggal " + (jumlah-1));
+			jumlah --;
+
+		}
+		
+		System.out.println("Anak ayam turun 1 mati satu tinggal induknya");
+	
+	
+	}
+
+}
+
---
+++ src/DeretBilanganGanjil.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class DeretBilanganGanjil {
+	 public static void main(String[] args) {
+	       Scanner bilangan = new Scanner(System.in);
+	       int ganjil,awal,akhir,jumlah=0;
+	       System.out.print("Masukkan Nilai Awal : ");awal= bilangan.nextInt();
+	       System.out.print("Masukkan Nilai Akhir : ");akhir= bilangan.nextInt();  
+	       System.out.print("Deret Bilangan Ganjilnya = ");
+	       for(ganjil=awal;ganjil<=akhir;ganjil++){
+	       if (ganjil % 2==1)
+	       {
+	           jumlah=jumlah+ganjil;
+	           System.out.print(ganjil + " ");
+	       }}
+	       System.out.println();
+	        System.out.println("Jumlah Deret Bilangan Ganjil Adalah = "+ jumlah + " ");
+	    }}
+
